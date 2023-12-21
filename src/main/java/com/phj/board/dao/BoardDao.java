5c10a860481776cf2b01e4c91dc3efa6d8c3a470
oopoo123 <oopoo123@naver.com>
2023-12-21 16:23:30 +0900

    v1.1.

--- src/main/java/com/phj/board/dao/BoardDao.java
+++ src/main/java/com/phj/board/dao/BoardDao.java
@@ -1,7 +1,10 @@
 package com.phj.board.dao;
 
+import java.sql.Connection;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class BoardDao {
@@ -9,15 +12,26 @@ public class BoardDao {
 	DataSource dataSource;
 	
 	public BoardDao() {
-		
-		Context context = new InitialContext();
-		context.lookup("java:comp/env/jdbc/oracledb");
-	} catch (NamingException)
-
-
-	public void write(String bname, String btitle, String bcontent) {
-		
+	
+		try {
+			Context context = new InitialContext();
+			context.lookup("java:comp/env/jdbc/oracledb");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
 		
 	}
+	
+	
+		public void write(String bname, String btitle, String bcontent) {
+			
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			
+			try {
+				stmt = conn.prepareStatement(sql);
+				
+			}
+		}
 
 }
